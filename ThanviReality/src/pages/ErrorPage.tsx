c2cb02e3ef0520df65cb233c36ab03e2dc2f3a7d
megha-kola <meghanasp87@gmail.com>
2025-06-04 18:51:07 +0530

    some stying update

--- ThanviReality/src/pages/ErrorPage.tsx
+++ ThanviReality/src/pages/ErrorPage.tsx
@@ -1,6 +1,6 @@
 const ErrorPage = () => {
-  return <div className="flex flex-col items-center justify-center h-screen gap-4">
-    <h1 className="text-4xl font-bold dark:text-white">Some <mark>technical glitch</mark> has occured.</h1>
+  return <div className="flex flex-col items-center justify-center h-screen gap-4 p-4 text-center">
+    <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold dark:text-white">Some <mark>technical glitch</mark> has occured.</h1>
     <a href="@" className="text-blue-500 underline">Please contact the developer</a>
   </div>;
 };
